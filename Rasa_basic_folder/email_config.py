9dd5c57fac2c49cb7c8c953b5578d64a74676aa4
srilakshmimaturi <42137485+srilakshmimaturi@users.noreply.github.com>
2021-02-28 16:08:42 +0530

    Update email_config.py

--- Rasa_basic_folder/email_config.py
+++ Rasa_basic_folder/email_config.py
@@ -1,6 +1,6 @@
 
 def Config():
-	gmail_user = 'srilakshmimaturi@gmail.com' # Gmail Username
-	gmail_pwd = 'Newpal@2021' #Gmail Password
+	gmail_user = '' # Gmail Username
+	gmail_pwd = '' #Gmail Password
 	gmail_config = (gmail_user, gmail_pwd)
 	return gmail_config
